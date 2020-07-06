6f886294c73f6dc70dba5ad0246dc6d797f62f32
Priyank <ppraja20@my.centennialcollege.ca>
2020-07-06 15:21:34 -0400

    Initial CreateJS Framework

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -1,46 +1,34 @@
 //Immediate Invoked Anonymous Function
-
-(function() {
-
+(function () {
     //Global Game Variables
-    let canvas = document.getElementById("canvas");
-    let stage:createjs.Stage;
-    let helloLabel:createjs.Text;
-    
-    function Init(){
+    var canvas = document.getElementById("canvas");
+    var stage;
+    var helloLabel;
+    function Init() {
         console.log("Initializing Start");
         Start();
     }
-
-    function Start(){
+    function Start() {
         console.log("Starting Application...");
-
         //Initialize CreateJS
         stage = new createjs.Stage(canvas);
         createjs.Ticker.framerate = 60;
         createjs.Ticker.on("tick", Update);
         Main();
     }
-
-    function Update(){
+    function Update() {
         stage.update();
-
         //Movement here
         helloLabel.scaleX += 0.001;
-        helloLabel.scaleY += 0.001;     
+        helloLabel.scaleY += 0.001;
     }
-
-    function Main(){
+    function Main() {
         console.log("Game Start");
-
         helloLabel = new createjs.Text("Hello World!", "40px Consolas", "#000000");
         helloLabel.x = 100;
         helloLabel.y = 100;
-
         stage.addChild(helloLabel);
     }
-
-
-    window.onload =Init;
-
+    window.onload = Init;
 })();
+//# sourceMappingURL=game.js.map
